9e927fb9204dd44c2b97491859e3b5b5d646531b
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-08-07 11:05:25 -0400

    Add constants for links and social icons

---
+++ src/constants/links.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    path: "/",
+    text: "home",
+  },
+  {
+    path: "/contact",
+    text: "contact",
+  },
+  {
+    path: "/articles",
+    text: "articles",
+  },
+]
---
+++ src/constants/social.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: "https://facebook.com",
+  },
+  {
+    icon: <FaTwitter />,
+    url: "https://twitter.com",
+  },
+  {
+    icon: <FaLinkedin />,
+    url: "https://linkedin.com",
+  },
+]
