fa99dea12c8a8233354f56aab8ddaaad3b7dc0aa
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-08-08 23:01:07 -0400

    Style custom error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,22 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "../css/error.module.css"
+import { Link } from "gatsby"
+import Banner from "../components/banner"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <header className={styles.error}>
+      <Banner>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <Link to="/" className="btn-white">
+          Home
+        </Link>
+      </Banner>
+    </header>
   </Layout>
 )
 
